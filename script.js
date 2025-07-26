b45ebcf7984fcb15286fbbb44a4606b4131166ba
MARYO JOAQUIM <achimmarius1972@gmail.com>
2025-07-26 21:41:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -303,6 +303,4 @@ document.addEventListener('DOMContentLoaded', () => {
         applyTranslations();
     });
 
-    // Aplică traducerile inițiale la încărcarea paginii
-    applyTranslations();
-});
+  
